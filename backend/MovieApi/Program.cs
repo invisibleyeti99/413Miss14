94f1286a42da9f7c44e1ce52636dbe4c0f3ccd61
Matthew Davies <mattjdavies199@gmail.com>
2023-04-08 11:02:13 -0600

    Final

--- backend/MovieApi/Program.cs
+++ backend/MovieApi/Program.cs
@@ -22,7 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors(p=> p.WithOrigins("http://localhost:3000/first"));
+app.UseCors(p=> p.WithOrigins("http://localhost:3000"));
 
 app.UseHttpsRedirection();
 
